7d53639f5b1c7ac57b2b7a1dad69cc5366789107
Shelmith Muthoni <muthonishelmith136@gmail.com>
2025-04-21 07:38:19 +0300

    Printing a list of the fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  # Print an empty list for length = 0
+        return
+
+    fib = [0, 1]
+    while len(fib) < length:
+        fib.append(fib[-1] + fib[-2])  # Fix indentation here
+
+    print(fib[:length])  # Print the Fibonacci sequence
+
+# Example usage
+print_fibonacci(9)
